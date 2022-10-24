9483e6387b997db9b764245668c611bb50b0aacb
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2022-10-24 17:35:32 +0300

    Feature (about.html page): included in the base html, page added and styled as per the figma design

--- FrontendMicrotask/urls.py
+++ FrontendMicrotask/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 #now import the views.py file into this code
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[    
   path('',views.welcome)
-]  
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
